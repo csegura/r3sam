68c03bbfe8a38244242220b2fc522922aa44ed74
carlos segura <csegura@ideseg.com>
2010-09-20 23:52:37 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R3sam::Application.config.secret_token = '8c88ea6781502e1651912f35b9b1b27c7d915723b8e30c3bfca996e9bee99af3703c5f9cd49f6e502609c42d94a4092656afe5a1fbe55238ae890d720b4fee65'
